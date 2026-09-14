f941b4b8d63b70db613fe5864fd4cb2e55583a59
Harsh Mandan <harshsinghmandan@gmail.com>
2026-09-14 09:46:50 +0530

    add failing specs for bind:this definite-assignment and runes event strictness

---
+++ fixtures/bugs/302-runes-untyped-dispatcher-events-stay-lax/Child.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+    dispatch('foo');
+    let w = $state(0);
+</script>
+
+<p>{w}</p>
---
+++ fixtures/bugs/302-runes-untyped-dispatcher-events-stay-lax/input.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Child from './Child.svelte';
+    const c = new Child({ target: document.body });
+    c.$on('bar', () => {});
+</script>
